37d95592c5deab5aa9078fc72c9d785d26483323
bbrintle <bbrintle@gmail.com>
2020-09-15 19:09:03 -0700

    finised up app. deleted files that I didnt need

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,6 @@ function pullAPI(cityName){
         url: queryURL,
         method: "GET"
         }).then(function(response) {
-            console.log(response);
             newCityObject = getLocationWeatherData(response);
             return newCityObject;
         });
@@ -27,15 +26,13 @@ function pull5DayAPI(cityName){
         url: queryURL,
         method: "GET"
         }).then(function(response) {
-            console.log(response);
-            //display first forcasted day
+            
             for (var i = 0; numOfForecastBoxes < 5; i += 8){
                 var newForecastDiv = $("<div>");
                 var daysOut = response.list[i].dt_txt.slice(0,10);
                 var getDate = daysOut.slice(5, -3) + "/" + daysOut.slice(8) + "/" + daysOut.slice(0,4);
                 var dateElement = $("<h5>");
                 dateElement.text(getDate);
-
 
                 var weatherDescription = response.list[i].weather[0].description;
                 var descriptionTag = $("<p>");
@@ -74,6 +71,11 @@ function clearSearchInput(){
     cityInput.val("");
 };
 
+function clearUVIndex(){
+    $('#display-uv-index').empty();
+    $('#display-uv-index').text("UV Index:")
+};
+
 function getLocationWeatherData(element){
     fahrenheit = calcFahrenheit(element.main.temp);
     humidity = element.main.humidity;
@@ -95,6 +97,8 @@ function getLocationWeatherData(element){
 };
 
 function displayWeatherInfo(cityObject){
+    clearUVIndex()
+
     var cityText = $('#city-text');
     cityText.text(cityObject.cityName + " (" +  moment().format('MM/DD/YYYY') + ")");
 
@@ -106,15 +110,33 @@ function displayWeatherInfo(cityObject){
 
     var displayWindSpeed= $('#display-wind-speed');
     displayWindSpeed.text("Wind Speed: " + cityObject.windSpeed);
+
+    uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + cityObject.lat + "&lon=" + cityObject.log + "&appid=07a5be9b6278338d5fd01e132774742f";
+    $.ajax({
+        url: uvIndexURL,
+        method: "GET"
+        }).then(function(response) {
+            var uvDiv = $('<div>');
+            uvDiv.text(response.value)
+            
+            if(response.value > 0 && response.value <= 2){ uvDiv.attr("class", 'bckColorGreen uv-box') };
+            if(response.value > 2 && response.value <= 4){ uvDiv.attr("class", 'bckColorYellow uv-box') };
+            if(response.value > 4 && response.value <= 6){ uvDiv.attr("class", 'bckColorOrange uv-box') }; 
+            if(response.value > 6 && response.value <= 9){ uvDiv.attr("class", 'bckColorRed uv-box') };
+            if(response.value > 9){ uvDiv.attr("class", 'bckColorPink uv-box') };
+
+            $('#display-uv-index').append(uvDiv);
+        });
+
 }
 
 searchBtn.addEventListener('click', function() {
     if(cityInput.val()){
-        if(currentCityArray.indexOf(cityInput.val()) !== -1){
+        if(currentCityArray.indexOf(cityInput.val().toLowerCase()) !== -1){
             //do nothing because it is already on the list.
         }else {
             
-            currentCityArray.push(cityInput.val());
+            currentCityArray.push(cityInput.val().toLowerCase());
             var newCityDiv = document.createElement('div');
             newCityDiv.textContent = cityInput.val();
             newCityDiv.classList.add('city-box');
@@ -133,6 +155,6 @@ $("#city-list").on("click", function(event){
     event.preventDefault();
     var targetClicked = event.target;
     var cityClicked = targetClicked.textContent;
-    pullAPI(cityClicked)
-    pull5DayAPI(cityClicked)
+    pullAPI(cityClicked);
+    pull5DayAPI(cityClicked);
 });
